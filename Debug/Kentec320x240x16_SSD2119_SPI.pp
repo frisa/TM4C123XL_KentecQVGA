08d06f6ecac8643fd33509d01b397f4df6b919e7
root <root@jan-PC.localdomain>
2016-05-28 17:56:53 +0200

    The running version

---
+++ Debug/Kentec320x240x16_SSD2119_SPI.pp
@@ -0,0 +1,35 @@
+# FIXED
+
+Kentec320x240x16_SSD2119_SPI.obj: ../Kentec320x240x16_SSD2119_SPI.c
+Kentec320x240x16_SSD2119_SPI.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdint.h
+Kentec320x240x16_SSD2119_SPI.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdbool.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/SW-TM4C-GRL-2.1.2.111/inc/hw_types.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/ssi.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/timer.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/rom.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h
+Kentec320x240x16_SSD2119_SPI.obj: /root/Development/SW-TM4C-GRL-2.1.2.111/grlib/grlib.h
+Kentec320x240x16_SSD2119_SPI.obj: ../Kentec320x240x16_SSD2119_SPI.h
+
+../Kentec320x240x16_SSD2119_SPI.c: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdint.h: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdbool.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h: 
+/root/Development/SW-TM4C-GRL-2.1.2.111/inc/hw_types.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/ssi.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/timer.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/rom.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h: 
+/root/Development/SW-TM4C-GRL-2.1.2.111/grlib/grlib.h: 
+../Kentec320x240x16_SSD2119_SPI.h: 
---
+++ Debug/ustdlib.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+ustdlib.obj: ../ustdlib.c
+ustdlib.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdint.h
+ustdlib.obj: /root/Development/SW-TM4C-GRL-2.1.2.111/driverlib/debug.h
+ustdlib.obj: /root/Development/TivaWare_C_Series-2.1.2.111/utils/ustdlib.h
+ustdlib.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdarg.h
+ustdlib.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/time.h
+ustdlib.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/linkage.h
+
+../ustdlib.c: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdint.h: 
+/root/Development/SW-TM4C-GRL-2.1.2.111/driverlib/debug.h: 
+/root/Development/TivaWare_C_Series-2.1.2.111/utils/ustdlib.h: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/time.h: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.2.LTS/include/linkage.h: 
